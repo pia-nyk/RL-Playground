aab7c67df1a454e5881e6b61395195ee223570c0
pia-nyk <nayakprya123@gmail.com>
2020-11-20 22:21:59 +0530

    windy grid world

--- SARSA/windy-gridworld-env.py
+++ SARSA/windy-gridworld-env.py
@@ -1,65 +1,51 @@
 from collections import defaultdict
 import numpy as np
+import matplotlib.pyplot as plt
 
-ROWS = 5
-COLS = 5
-
-def policy(env, state, ep, Q):
-    if np.random.rand() > ep:
-        get_action_with_max_val([Q[state, a] for a in env.act_space])
+def policy(env, st, eps, Q):
+    if np.random.rand() > eps:
+        return get_action_with_max_val([Q[st,a] for a in env.act_space])
     else:
         return np.random.choice(env.act_space)
 
 def get_action_with_max_val(action_vals):
     return np.random.choice(np.flatnonzero(action_vals == np.max(action_vals)))
 
-class NormalGridWorld:
+class WindyGridWorld:
     def __init__(self):
         self.act_space = [0,1,2,3]
         self.reset()
 
     def reset(self):
         self.x = 0
-        self.y = 0
-        return (0,0)
+        self.y = 3
+        return (0,3)
 
     def step(self, action):
         self.x, self.y = self.transition(action)
-        if self.x == 4 and self.y == 4:
+        if self.x == 7 and self.y == 3:
             return (self.x, self.y), 0, True
         return (self.x, self.y), -1, False
 
     def transition(self, action):
         x = self.x
         y = self.y
-        if action == 0:
-            x-=1
-        elif action == 1:
-            x+=1
-        elif action == 2:
-            y-=1
-        elif action == 3:
-            y+=1
-        if x >= 0 and x <5:
+        if self.x in [3,4,5,8]: y-=1
+        elif self.x in [6,7]: y-=2
+
+        if action == 0: x-=1
+        elif action == 1: x+=1
+        elif action == 2: y-=1
+        elif action == 3: y+=1
+        if x >= 0 and x <= 9:
             self.x = x
-        if y >=0 and y < 5:
+        if y >=0 and y <= 6:
             self.y = y
         return self.x, self.y
 
-def evaluate_policy(env, Q):
-    S = env.reset()
-    path = [S]
-    done = False
-    while True:
-        action = policy(env,S,0,Q)
-        S, reward, done = env.step(action)
-        path.append(S)
-        print(path)
-        if done:
-            break
-    return path
 
-def sarsa(env, ep, alpha, n_episodes, gamma=1.0):
+def sarsa(env, ep, alpha, n_episodes, max_steps, gamma=1.0):
+    hist_ep = []
     Q = defaultdict(float)
     for _ in range(n_episodes):
         if(_%500 == 0):
@@ -72,12 +58,23 @@ def sarsa(env, ep, alpha, n_episodes, gamma=1.0):
             Q[S,A] = Q[S,A] + alpha * (reward + gamma*Q[S_,A_] - Q[S,A])
             S = S_
             A = A_
+
+            hist_ep.append(_)
+            if max_steps is not None:
+                if len(hist_ep) >= max_steps:
+                    return Q, hist_ep
             if done:
                 break
-    return Q
+    return Q, hist_ep
 
-env = NormalGridWorld()
-Q = sarsa(env, 0.1, 0.5, 10000)
-print(Q)
-# optimal_path = evaluate_policy(env, Q)
-# print (optimal_path)
+env = WindyGridWorld()
+Q, hist_ep = sarsa(env, 0.1, 0.5, 1000, 8000)
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(hist_ep, color='red')
+ax.set_xlabel('Time Steps')
+ax.set_ylabel('Episodes')
+plt.tight_layout()
+plt.show()
